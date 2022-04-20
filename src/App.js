27d44e5746dff5f9963a8e8b8332f0e79b328524
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-04-20 10:24:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useReducer } from 'react'
 
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+}
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'increment':
+    case ACTIONS.INCREMENT:
       return { count: state.count + 1 }
-    case 'decrement':
+    case ACTIONS.DECREMENT:
       return { count: state.count - 1 }
     default:
       return state
@@ -15,11 +20,11 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, { count: 0 })
 
   const increment = () => {
-    dispatch({ type: 'increment' })
+    dispatch({ type: ACTIONS.INCREMENT })
   }
 
   const decrement = () => {
-    dispatch({ type: 'decrement' })
+    dispatch({ type: ACTIONS.DECREMENT })
   }
 
   return (
